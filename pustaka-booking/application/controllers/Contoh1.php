99c14e7d0e07931ae3626af4c982b91be39b4a92
daffaalbazhilli <daffa2603albazhilli@gmail.com>
2024-04-02 14:56:42 +0700

    reupload file tugas

---
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya Muhammad Adil Saputra
+        Saya tinggal di daerah Pengasinan";
+    }
+}
